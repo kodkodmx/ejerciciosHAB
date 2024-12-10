09eb10aa1b1f5b20b074b06d7b8cd591b37ae8f5
kodkodmx <alberto.haboba@gmail.com>
2024-12-09 18:48:31 -0600

    Añadir Ejercicio Tipos Datos

---
+++ 01-Java-Basico/TiposDeDatos/src/tiposdedatos/TiposDeDatos.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tiposdedatos;
+
+/**
+ *
+ * @author betoh
+ */
+public class TiposDeDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int edad = 33;
+        String nombre = "Alberto";
+        char letra = 'a';
+        boolean bandera = true;
+        double estatura = 1.67;
+        
+        edad = 'a';
+        letra = 33;
+        bandera = 0;
+        estatura = 33;
+        
+        System.out.println("Hola Alberto, Bienvenido al sistema");
+        System.out.println("Hola " + nombre+ ", Bienvenido al sistema");
+        
+        
+    }
+    
+}
