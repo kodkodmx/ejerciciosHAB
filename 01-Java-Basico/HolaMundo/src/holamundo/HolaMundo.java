288ed9c836144cceb0629d07a3ecaa40e71ac02f
kodkodmx <alberto.haboba@gmail.com>
2024-12-10 13:39:02 -0600

    Corregir hola mundo

--- 01-Java-Basico/HolaMundo/src/holamundo/HolaMundo.java
+++ 01-Java-Basico/HolaMundo/src/holamundo/HolaMundo.java
@@ -16,7 +16,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola Mundo");
-        System.out.println("ALGO MAS QUE DIJO");
+        System.out.println("Probando un hola mundo diferente");
     }
 
 }
